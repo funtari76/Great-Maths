40b945621d49cb8c2a5a25490845a241a376e548
funtari76 <funtari777@gmail.com>
2022-04-20 03:01:28 +0000

    Display question to the user

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,10 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
               } else{
                   let gameType = this.getAttribute("data-type");
-                alert('You clicked ${gameType}');  
+                  runGame(gameType);  
               }  
           })
      }
+     runGame("addition");
 })
 
 /**
@@ -20,11 +21,18 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
   // Creates two numbers betweenn 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    
+    if (gameType === "addition") {
+      displayAdditionQuestion(num1, num2);
+    } else {
+      alert('Unknown game type: ${gameType}');
+      throw 'Unknown game type: ${gameType}. Aborting!';
+    }
 }
 
 
@@ -44,8 +52,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('opernad1').textContent = operand1;
+    document.getElementById('opernad2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displayASubtractQuestion() {
